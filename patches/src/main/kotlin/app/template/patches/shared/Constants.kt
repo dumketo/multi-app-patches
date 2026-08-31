b84a86901a09b2fc0f90760cae354db037c66e4c
Hasan Ahmed Jobayer <dumketo99@gmail.com>
2026-08-31 20:45:06 +0600

    Add/Update patches/src/main/kotlin/app/template/patches/shared/Constants.kt

--- patches/src/main/kotlin/app/template/patches/shared/Constants.kt
+++ patches/src/main/kotlin/app/template/patches/shared/Constants.kt
@@ -14,3 +14,39 @@ val WALLVERSE_COMPATIBILITY = Compatibility(
         "wallverseCustomBrandingPatch"
     )
 )
+
+val ACCUBATTERY_COMPATIBILITY = Compatibility(
+    name = "AccuBattery",
+    packageName = "com.digibites.accubattery",
+    apkFileType = ApkFileType.APK,
+    minVersion = "2.1.8",
+    maxVersion = "2.1.8",
+    patches = listOf(
+        "accubatteryUnlockProPatch",
+        "accubatteryRemoveAdsPatch"
+    )
+)
+
+val ADGUARD_COMPATIBILITY = Compatibility(
+    name = "AdGuard",
+    packageName = "com.adguard.android",
+    apkFileType = ApkFileType.APK,
+    minVersion = "4.14.68",
+    maxVersion = "4.14.68",
+    patches = listOf(
+        "adguardPremiumLicensePatch",
+        "adguardCustomDnsPatch"
+    )
+)
+
+val ADOBE_ACROBAT_COMPATIBILITY = Compatibility(
+    name = "Adobe Acrobat",
+    packageName = "com.adobe.reader",
+    apkFileType = ApkFileType.XAPK,
+    minVersion = "26.7.0.47169",
+    maxVersion = "26.7.0.47169",
+    patches = listOf(
+        "adobeUnlockProToolsPatch",
+        "adobeRemoveWatermarksPatch"
+    )
+)
